ac9625ed344be56fe898ceeb420e8627cf96c647
Feng Zhang <prod.feng@gmail.com>
2024-07-12 23:09:44 -0400

    Update slurm_nodes_perf.py
    List all nodes. Only list one user job for shared queue/node for simplicity.

--- slurm_nodes_perf.py
+++ slurm_nodes_perf.py
@@ -2,7 +2,7 @@
 import subprocess
 
 # Find out <10% loaded nodes...
-sinfo_cmd="/cm/shared/apps/slurm/current/bin/sinfo -a --Node -o '%.10N %8O %c %.10mMB %.10eMB %.5a %.6t %12E %G'|uniq|grep alloc|grep -v -e a100|awk '{if (($2/$3)<0.3||($2/$3)>1.1) print $1,int($2/$3*100), $3, $7}'|sort -n -k2"
+sinfo_cmd="/cm/shared/apps/slurm/current/bin/sinfo -a --Node -o '%.10N %8O %c %.10mMB %.10eMB %.5a %.6t %12E %G'|uniq|grep alloc|grep -v -e a100|awk '{if (($2/$3)<100.3||($2/$3)>0.01) print $1,int($2/$3*100), $3, $7}'|sort -n -k2"
 
 # With format of: node  load  CPUs Status(alloc) 
 result = subprocess.getoutput(sinfo_cmd)
@@ -19,7 +19,7 @@ for line in result.split("\n"):
     #print("Found node: ",node, " with Load: ",load, " CPUS: ",cpus)
     squeue_cmd="/cm/shared/apps/slurm/current/bin/squeue -t r -o '%10i %22P %16j %12u %.10M %10l %.4D %20R' -ahw " + node
     #print("squeue_cmd ",squeue_cmd)
-    job = subprocess.getoutput(squeue_cmd)
+    job = subprocess.getoutput(squeue_cmd).split("\n")[0]
     #print("Found job: ")
     print(" %5.f"% float(load)+"%    "+cpus+"  "+node+" "+job)
 
